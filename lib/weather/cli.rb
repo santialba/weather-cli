5ed5aa4576a75fcb9f999384a77d8f47537c79ea
Santiago J. Alba <santij.alba@gmail.com>
2020-11-27 17:10:41 +0100

    Adding functionality to weather cli

--- lib/weather/cli.rb
+++ lib/weather/cli.rb
@@ -13,24 +13,32 @@ module Weather
   module Cli
     DIVISION_CODE = 102
     class Error < StandardError; end
-    def self.init
-      options = Menu.new(ARGV).prompt
-      response = HttpClient.new({ division: DIVISION_CODE }).api_request
-      location_id = Parser.new(response.body).retrieve_id_from_location(options['location'])
-      response = HttpClient.new({ localidad: location_id }).api_request
-      result(Parser.new(response.body), options)
-    end
-
-    def self.result(parser, options)
-      if options['-today']
-        puts "Temperature of the day between #{parser.max_temperatures.first}º-#{parser.min_temperatures.first}º"
+    class << self
+      def init
+        options = Menu.new(argv).prompt
+        response = HttpClient.new({ division: DIVISION_CODE }).api_request
+        location_id = Parser.new(response.body).retrieve_id_from_location(options['location'])
+        response = HttpClient.new({ localidad: location_id }).api_request
+        result(Parser.new(response.body), options)
       end
-      puts "Average of #{average(parser.max_temperatures)}º maximum temperature during the week" if options['-av_max']
-      puts "Average of #{average(parser.min_temperatures)}º minimum temperature during the week" if options['-av_min']
-    end
 
-    def self.average(temperatures)
-      temperatures.sum / temperatures.size
+      private
+
+      def argv
+        ARGV
+      end
+
+      def result(parser, options)
+        if options['-today']
+          puts "Temperature of the day between #{parser.max_temperatures.first}º-#{parser.min_temperatures.first}º"
+        end
+        puts "Average of #{average(parser.max_temperatures)}º maximum temperature during the week" if options['-av_max']
+        puts "Average of #{average(parser.min_temperatures)}º minimum temperature during the week" if options['-av_min']
+      end
+
+      def average(temperatures)
+        temperatures.sum / temperatures.size
+      end
     end
   end
 end
--- spec/weather/cli_spec.rb
+++ spec/weather/cli_spec.rb
@@ -1,9 +1,17 @@
+# frozen_string_literal: true
+
 RSpec.describe Weather::Cli do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Weather::Cli::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#init' do
+    subject { described_class }
+
+    it 'call correctly result method' do
+      allow(subject).to receive(:argv).and_return(['-today', 'Gava'])
+      expect(subject).to receive(:result).once
+      subject.init
+    end
   end
 end
